95a6670a3d9ccfcdc9bf39f17cb40d1d63cad826
Jesús Tejada <tejadong@gmail.com>
2021-01-24 14:23:41 +0100

    Fix mensaje

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {ChangeDetectorRef, Component, OnInit} from '@angular/core';
 import { ElectronService } from 'ngx-electron';
 
 @Component({
@@ -8,22 +8,25 @@ import { ElectronService } from 'ngx-electron';
 })
 export class HomeComponent implements OnInit {
   title = 'integrate-angular-electron';
+  mensaje = '';
   ipcRenderer = this.electronService.ipcRenderer;
   showNotification = false;
   showRestartBtn = false;
 
-  constructor(private electronService: ElectronService) {
+  constructor(private electronService: ElectronService, private cd: ChangeDetectorRef) {
     this.ipcRenderer.on('update_available', () => {
       this.ipcRenderer.removeAllListeners('update_available');
-      console.log('Hay una nueva actualización disponible. Descargando...');
+      this.mensaje = 'Hay una nueva actualización disponible. Descargando...';
       this.showNotification = true;
+      this.cd.detectChanges();
     });
 
     this.ipcRenderer.on('update_downloaded', () => {
       this.ipcRenderer.removeAllListeners('update_downloaded');
-      console.log('Actualización descargada. Será instalada en el próximo reinicio. ¿Desea reiniciar la apliación?');
+      this.mensaje = 'Actualización descargada. Será instalada en el próximo reinicio. ¿Desea reiniciar la apliación?';
       this.showRestartBtn = true;
       this.showNotification = true;
+      this.cd.detectChanges();
     });
   }
 
@@ -42,6 +45,7 @@ export class HomeComponent implements OnInit {
 
   closeNotification(): void {
     this.showNotification = false;
+    this.cd.detectChanges();
   }
 
   restartApp(): void  {
